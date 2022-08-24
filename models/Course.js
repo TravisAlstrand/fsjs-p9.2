e1ab3397cec3cdb968c68c3829e3fd8cf55ce56d
Travis Alstrand <travisalstrand@gmail.com>
2022-08-23 20:44:30 -0700

    Commit 3

---
+++ models/Course.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = ( sequelize ) => {
+
+    class Course extends Model {}
+
+    Course.init({
+        title: {
+            type: DataTypes.STRING
+        },
+        description: {
+            type: DataTypes.TEXT
+        },
+        estimatedTime: {
+            type: DataTypes.STRING
+        },
+        materialsNeeded: {
+            type: DataTypes.STRING
+        }
+    }, { sequelize });
+
+};
+
+return Course;
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = ( sequelize ) => {
+
+    class User extends Model {}
+
+    User.init({
+        firstName: {
+            type: DataTypes.STRING
+        },
+        lastName: {
+            type: DataTypes.STRING
+        },
+        emailAddress: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.STRING
+        }
+    }, { sequelize });
+
+};
+
+return User;
